4ca8f4c0345f6a54309d6b67ed71b7d93c9b0ee7
Gabriel Dolzan <gabrieldolzan@icloud.com>
2025-04-11 03:52:17 -0300

    Cheguei na parte de adicionar os constraints, agora só preciso ver um jeito bom de organizar isso nos vectors.

--- WFC_Input.cpp
+++ WFC_Input.cpp
@@ -12,6 +12,25 @@ void WFC_Input::initialize_variables()
     constraints_map = {};
 }
 
+bool WFC_Input::find_in_set(std::string element, std::string err_msg)
+{
+    if (tiles_set.find(element) == tiles_set.end())
+    {
+        std::cerr << err_msg << ": " << element << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool WFC_Input::compare_line(std::string section, std::string line, std::string err_msg)
+{
+    if (section != line)
+    {
+        std::cerr << err_msg << std::endl;
+        return false;
+    }
+    return true;
+}
 
 bool WFC_Input::file_exists(const std::string& filename)
 {
@@ -30,28 +49,18 @@ bool WFC_Input::file_exists(const std::string& filename)
 bool WFC_Input::read_tiles(std::ifstream &file)
 {
     std::string line;
-    std::streampos pos;
 
     std::getline(file, line);
-
-    if (line != "[Tiles]")
-    {
-        std::cerr << "Didn't find [Tiles] Header, returning empty Tile Set" << std::endl;
+    if (!compare_line("[Begin]", line, "Didn't find [Begin] Header inside [Tiles]"))
         return false;
-    }
 
     while (true)
     {
         if (!std::getline(file, line))
             break;
 
-        pos = file.tellg();
-
-        if (line == "[Constraints]")
-        {
-            file.seekg(pos);
+        if (line == "[End]")
             break;
-        }
 
         auto result = tiles_set.insert(line);
         if (!result.second)
@@ -63,15 +72,46 @@ bool WFC_Input::read_tiles(std::ifstream &file)
     return true;
 }
 
-bool read_constraints(std::ifstream &file)
+bool WFC_Input::read_constraints(std::ifstream &file)
 {
     std::string line;
     std::getline(file, line);
 
-    if (line != "[Constraints]")
-    {
-        std::cerr << "Didn't find [Tiles] Header, returning empty Tile Set" << std::endl;
+    if (!compare_line("[Begin]", line, "Didn't find [Begin] Header inside [Constraints]"))
         return false;
+
+    while (true)
+    {
+        if (!std::getline(file, line))
+            break;
+
+        if (line == "[End]")
+            break;
+
+        std::istringstream iss(line);
+        std::vector<std::string> words;
+        std::string word;
+
+        while (iss >> word)
+        {
+            words.push_back(word);
+        }
+
+        // Agora words contem todas as palavras daquela linha, precisamos processá-las de acordo.
+        for (int i = 0; i < (int)words.size(); i++)
+        {
+            // i == 0, verificamos se a cor está cadastrada nos dados de Tiles
+            if (i == 0)
+            {
+                if (!find_in_set(words.at(i), "Trying to add constraints to an unexisting tile"))
+                    return false;
+            }
+            else if (i == 1) // i == 1, determinamos quais direções vão ser adicionadas
+            {
+                if (words.at(i) == "LURD")
+                    std::cerr << "Ok ta indo" << std::endl;
+            }
+        }
     }
 
     return true;
@@ -90,11 +130,13 @@ bool WFC_Input::read_file(const std::string& filename)
 
     std::getline(file, line);
 
-    if (line != "[WFCINPUT]")
-    {
-        std::cerr << "WFC_Input Couldn't find [WFCINPUT] header in: " << filename << "\n";
+    if (!compare_line("[WFCINPUT]", line, "Couldn't find [WFCINPUT]"))
         return false;
-    }
+
+    std::getline(file, line);
+
+    if (!compare_line("[Tiles]", line, "Couldn't find [Tiles]"))
+        return false;
 
     /* Reading the tiles */
     if (!read_tiles(file))
@@ -102,7 +144,14 @@ bool WFC_Input::read_file(const std::string& filename)
 
     std::cout << "Number of tiles in set: " << tiles_set.size() << std::endl;
 
+    std::getline(file, line);
+
+    if (!compare_line("[Constraints]", line, "Couldn't find [Constraints]"))
+        return false;
+
     /* Now Reading the Constraints */
+    if (!read_constraints(file))
+        return false;
 
     file.close();
     return true;
